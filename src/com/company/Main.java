3ac67f90804483c706e1e7df3a494fefeb79e83a
mak1115 <mak1115@swcsd.us>
2022-04-20 09:33:18 -0400

    Solution

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -32,54 +32,62 @@ public class Main {
         System.out.println("Number of permits: " + sem.availablePermits());*/
 
     }
-    public static int x = 0;
+    public static int x = 20;
+    static Semaphore sem = new Semaphore(4,true);//have them write the code without this first
+    //then alter the amount of permits available to 2. Fire rules only allow so many students in the restroom!?
 }
 
 class Restroom1 implements Runnable{
-
     public void run(){
-        for (int i = 0; i < 5; i++) {
+        for (int i = 0; i < Main.x; i++) {
+            try { Main.sem.acquire(1); } catch (InterruptedException e) { e.printStackTrace(); }
             System.out.println("Restroom 1 in use");
             try { Thread.sleep(2125); } catch (InterruptedException e) { e.printStackTrace(); }
             System.out.println("Restroom 2 available");
             Main.x-=1;
             System.out.println(Main.x);
+            Main.sem.release(1);
         }
-
     }
-
 }
 
 class Restroom2 implements Runnable{
     public void run(){
-        for (int i = 0; i < 5; i++) {
+        for (int i = 0; i < Main.x; i++) {
+            try { Main.sem.acquire(1); } catch (InterruptedException e) { e.printStackTrace(); }
             System.out.println("Restroom 2 in use");
             try { Thread.sleep(3500); } catch (InterruptedException e) { e.printStackTrace(); }
             System.out.println("Restroom 2 available");
-            Main.x+=1;
+            Main.x-=1;
             System.out.println(Main.x);
+            Main.sem.release(1);
         }
     }
 }
 class Restroom3 implements Runnable{
     public void run(){
-        for (int i = 0; i < 5; i++) {
+        for (int i = 0; i < Main.x; i++) {
+            try { Main.sem.acquire(1); } catch (InterruptedException e) { e.printStackTrace(); }
             System.out.println("Restroom 3 in use");
-            try { Thread.sleep(2000); } catch (InterruptedException e) { e.printStackTrace(); }
+            try { Thread.sleep(500); } catch (InterruptedException e) { e.printStackTrace(); }//adjusted the millis to 500 to see if time is dramatically reduced.
+            // Next alter the permits and say the bouncer only has 3 passes.
             System.out.println("Restroom 3 available");
-            Main.x+=1;
+            Main.x-=1;
             System.out.println(Main.x);
+            Main.sem.release(1);
         }
     }
 }
 class Restroom4 implements Runnable{
     public void run(){
-        for (int i = 0; i < 5; i++) {
+        for (int i = 0; i < Main.x; i++) {
+            try { Main.sem.acquire(1); } catch (InterruptedException e) { e.printStackTrace(); }
             System.out.println("Restroom 4 in use");
             try { Thread.sleep(2250); } catch (InterruptedException e) { e.printStackTrace(); }
             System.out.println("Restroom 4 available");
-            Main.x+=1;
+            Main.x-=1;
             System.out.println(Main.x);
+            Main.sem.release(1);
         }
     }
 }
@@ -87,7 +95,7 @@ class Restroom4 implements Runnable{
 class Timer implements Runnable{
     public void run(){
         for (int i = 1; i < 30; i++) {
-            System.out.println(i + "second");
+            System.out.println(i + " second");
             try { Thread.sleep(1000); } catch (InterruptedException e) { e.printStackTrace(); }
         }
     }
